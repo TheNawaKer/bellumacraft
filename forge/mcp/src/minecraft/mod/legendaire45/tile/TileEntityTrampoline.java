13585925e77bb3aecef78a75f4150874c1c3704b
legendaire45 <jimmy.furet@gmail.com>
2013-02-11 23:35:11 +0100

    modif mineur (blocktrampoline)
    detection d'un bug server mineur -> blockTrampoline ne fonctionne pas
    correctement sur server

--- forge/mcp/src/minecraft/mod/legendaire45/tile/TileEntityTrampoline.java
+++ forge/mcp/src/minecraft/mod/legendaire45/tile/TileEntityTrampoline.java
@@ -6,25 +6,4 @@ import net.minecraft.tileentity.TileEntity;
 
 
 public class TileEntityTrampoline extends TileEntity
-{
-    /**
-     * Reads a tile entity from NBT.
-     */
-    public void readFromNBT(NBTTagCompound par1NBTTagCompound)
-    {
-        super.readFromNBT(par1NBTTagCompound);
-    }
-
-    /**
-     * Writes a tile entity to NBT.
-     */
-    public void writeToNBT(NBTTagCompound par1NBTTagCompound)
-    {
-        super.writeToNBT(par1NBTTagCompound);
-    }
-
-    public void openChest(){}
-
-    public void closeChest(){}
-
-}
+{}
